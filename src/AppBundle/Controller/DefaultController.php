6afab4d9f1a5ee01703580dc6a8bbf45dbac12ed
Paweł Chrościcki <chroscickipawel.30@gmail.com>
2017-08-02 23:37:26 +0200

    During create Kursy page

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,17 +5,46 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\Tutorial;
+use AppBundle\Entity\Video;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
+    public function kursyAction()
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ));
+        $em=$this->getDoctrine()->getManager();
+        $tutorials=$em->getRepository('AppBundle:Tutorial')->findAll();
+        return $this->render('body/kursy.html.twig',['arr'=>$tutorials]);
     }
+
+    /**
+     * @Route("/o_mnie", name="o_mnie")
+     */
+    public function oMnieAction()
+    {
+        return $this->render('body/o_mnie.html.twig');
+    }
+
+    /**
+     * @Route("/koszyk", name="koszyk")
+     */
+    public function koszykAction()
+    {
+        return $this->render('body/koszyk.html.twig');
+    }
+
+    /**
+     * @Route("/{id}/tutorial")
+     */
+    public function tutorialByIdAction($id)
+    {
+        $em=$this->getDoctrine()->getManager();
+        $videos=$em->getRepository('AppBundle:Video')->findByTutorial($id);
+        return $this->render('body/videos.html.twig',['arr'=>$videos]);
+    }
+
+
 }
